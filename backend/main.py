43c043e4fb9ad2e6edffb43178b407dc3a5ea906
Akashneel <akashneelrc@gmail.com>
2024-02-15 01:31:09 +0530

    feat: basic flash

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,23 @@
 from ..model import predict
+from flask import Flask
 
 personal_model= predict.load_personal_model()
 home_model= predict.load_home_model()
+ 
+
+app = Flask(__name__)
+ 
+@app.route('/')
+def index():
+    return 'Server is up'
+
+@app.route('/loan/personal', methods=['POST'])
+def handle_info():
+    info={
+        'Education': request.form[Education], 
+        # ADD ALL THE INFO
+    }
+ 
+# main driver function
+if __name__ == '__main__':
+    app.run()
